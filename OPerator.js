3ad6ae3c259a673cba883c88d595109236119145
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-15 20:31:07 -0700

    Code Successfully

---
+++ OPerator.js
@@ -0,0 +1,109 @@
+
+/**** Activity 1 : Arethmetic Operators ****/
+
+let add1 = 10 ;
+let add2 = 20 ;
+console.log(add1 + add2)
+
+var substract1 = 30 ;
+var substract2 = 40 ;
+console.log(substract1 + substract2)
+
+const multiple1 = 5 ;
+const multiple2 = 6 ;
+console.log(multiple1 * multiple2)
+
+let divide1 = 60 ;
+let divide2 = 5 ;
+console.log(divide1 / divide2)
+
+const remin1 = 49 ;
+const remin2 = 7 ;
+console.log(remin1 % remin2)
+
+/**** Activity 2 : Assignment Operators ****/
+
+  let number1 = 75 ;
+  let addTwo = number1 + 10
+  if (addTwo===85) {
+    console.log(addTwo)
+  }    
+
+  let number2 = 100 ;
+  let sbtTwo = number2 - 10
+  if (sbtTwo==90) {
+    // let addTwo2 = number2 - 10
+    console.log(sbtTwo)
+  }    
+  
+
+/**** Activity 3 : Comparision Operators ****/
+
+let firstNum = 33 ;
+let secondNum = 19 ;
+
+if (firstNum > secondNum) {
+  console.log(`The number is greater ${firstNum}`)
+}
+else {
+  console.log(`The number is less ${secondNum}`)
+}
+
+
+let firstNum1 = 50 ;
+let secondNum1 = 36 ;
+
+if (firstNum1 >= secondNum1) {
+  console.log(`The condition is true ${firstNum1}`)
+}
+else {
+  console.log(`The condition is false ${secondNum1}`)
+}
+
+let firstNum2 = 60 ;
+let secondNum2 = 60 ;
+
+if (firstNum2 == secondNum2) {
+  console.log(`The condition is true ${firstNum2}`)
+}
+else {
+  console.log(`The condition is false ${secondNum2}`)
+}
+
+/**** Activity 4 : logical Operators ****/
+
+
+let firstNum3 = 60 ;
+let secondNum3 = 45 ;
+
+if (firstNum3 != secondNum3 && firstNum3 > secondNum3 ) {
+  console.log(`The condition is true `)
+}
+else {
+  console.log(`The condition is false`)
+}
+
+
+let firstNum4 = 90 ;
+let secondNum4 = 90 ;
+
+if (firstNum4 == secondNum4 || firstNum4 > secondNum4 || firstNum4 < secondNum4 ) {
+  console.log(`The condition is true `)
+}
+else {
+  console.log(`The condition is false`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
